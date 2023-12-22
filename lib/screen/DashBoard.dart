cafbdf75b65f02caba6aea96e711263f402ac0f1
HariPremRC <hari.prem24@gmail.com>
2023-12-22 13:11:09 -0800

    Second Commit(2)

--- lib/screen/DashBoard.dart
+++ lib/screen/DashBoard.dart
@@ -9,19 +9,37 @@ class DashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Dashboard".toUpperCase()),
-        backgroundColor: Colors.white10,
+        title: Text("DASHBOARD".toUpperCase()),
+        backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: IconButton(
-          icon: Icon(FontAwesomeIcons.bitcoin),
-          onPressed: () {},
-          iconSize: 100.0,
-          splashColor: Colors.red,
-          highlightColor: Colors.cyan,
+      body: Container(
+        height: 280.0,
+        width: 450.0,
+        padding: EdgeInsets.all(10.0),
+        margin: EdgeInsets.all(50.0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.white70,
+          border: Border.all(color: Colors.teal, width: 10.0),
+          //borderRadius: BorderRadius.circular(10.0),
+          shape: BoxShape.circle,
+          image: DecorationImage(image : AssetImage("images/krishnaimage.jpg")),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.green.shade800,
+              blurRadius: 5.0,
+              spreadRadius: 5.0,
+              offset: Offset(0,0),
+            )
+          ]
+          
         ),
+        // child: Text(
+        //   "Awesome",
+        //   style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        // ),
       ),
     );
   }
