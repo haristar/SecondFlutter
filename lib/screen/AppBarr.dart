8462cb2d61601fa800c0e9db3046137391d19753
HariPremRC <hari.prem24@gmail.com>
2023-12-23 13:43:33 -0800

    Listview Commit(1)

--- lib/screen/AppBarr.dart
+++ lib/screen/AppBarr.dart
@@ -6,58 +6,44 @@ class AppBarr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: false,
-      appBar: AppBar(
-        title: Text("ROWS AND COLUMNS"),
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(child: Image(image: AssetImage("images/krishnaimage.jpg"),width: 120.0,)),
-                Expanded(flex:2,child: Image(image: AssetImage("images/krishnaimage.jpg"),width: 120.0,)),
-                Expanded(child: Image(image: AssetImage("images/krishnaimage.jpg"),width: 120.0,)),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.star),
-                Icon(Icons.star),
-                Icon(Icons.star),
-                Icon(Icons.star_border),
-                Icon(Icons.star_border),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children : [
-                  Icon(Icons.phone, size: 30.0),
-                  Text("PHONE")
-                  ],
-                ),
-                Column(
-                  children : [
-                    Icon(Icons.alt_route, size: 30.0),
-                    Text("ROUTE")
-                  ],
-                ),
-                Column(
-                  children : [
-                    Icon(Icons.share, size: 30.0),
-                    Text("SHARE")
-                  ],
-                )
-
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+        body: Container(
+          child: ListView(
+            itemExtent: 100.0,
+            //padding: EdgeInsets.all(10.0),
+            reverse: false,
+            scrollDirection: Axis.vertical,
+            //shrinkWrap: true,
+            children: [
+              ListTile(
+                leading: CircleAvatar(child: Icon(Icons.alarm_on,size: 30.0,),backgroundColor: Colors.brown,),
+                title: Text("ALARM"),
+                subtitle: Text("Morning"),
+                trailing: Icon(Icons.add),
+                onTap: (){},
+              ),
+              ListTile(
+                leading: CircleAvatar(child: Icon(Icons.shopping_cart,size: 30.0,),backgroundColor: Colors.brown,),
+                title: Text("SHOPPING"),
+                subtitle: Text("Groceries"),
+                trailing: Icon(Icons.add),
+                onTap: (){},
+              ),
+              ListTile(
+                leading: CircleAvatar(child: Icon(Icons.backup,size: 30.0,),backgroundColor: Colors.brown,),
+                title: Text("BACKUP"),
+                subtitle: Text("Weekly"),
+                trailing: Icon(Icons.add),
+                onTap: (){},
+              ),
+              ListTile(
+                leading: CircleAvatar(child: Icon(Icons.wallet,size: 30.0,),backgroundColor: Colors.brown,),
+                title: Text("SAVING"),
+                subtitle: Text("Statement"),
+                trailing: Icon(Icons.add),
+                onTap: (){},
+              )
+            ],
+          ),
+    ));
   }
 }
