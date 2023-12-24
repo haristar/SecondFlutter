65082f695710f435334c9075091107c4e3047db4
HariPremRC <hari.prem24@gmail.com>
2023-12-24 14:59:08 -0800

    Stack Commit

--- lib/screen/AppBarr.dart
+++ lib/screen/AppBarr.dart
@@ -6,89 +6,57 @@ class AppBarr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white24,
       appBar: AppBar(
-        title: Text("FLOATING ACTION BAR",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "MY PROFILE",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey,
-        foregroundColor: Colors.black,
-        elevation: 5.0,
-        mini: true,
-        //   shape: BeveledRectangleBorder(
-        //     borderRadius: BorderRadius.circular(1.0),
-        //     side: BorderSide(color: Colors.black, width: 1.0),
-        // ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        notchMargin: 5.0,
-        shape: CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 0.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.home),
-                  Text("Home"),
-                ],
-              ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          constraints: BoxConstraints.expand(width: 300.0, height: 500.0),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.white24,
+                  offset: Offset(0, 2),
+                  spreadRadius: 5.0,
+                  blurRadius: 10.0),
+            ],
+            image: DecorationImage(
+              image: AssetImage("images/myimage.jpg"),
+              fit: BoxFit.cover,
             ),
-            Padding(
-              padding:
-              const EdgeInsets.only(right: 150.0,left: 60.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.shopping_cart,
-                   // color: Colors.white,
-                  ),
-                  Text(
-                    "Shop",
-                    //style: TextStyle(color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding:
-              const EdgeInsets.only(left: 0.0,right: 20.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.favorite,
-                  ),
-                  Text(
-                    "Fav",)
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 0.0,left: 40.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.settings,
-                    //color: Colors.white,
-                  ),
-                  Text(
-                    "Setting",
-                  )
-                ],
-              ),
-            ),
-          ],
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          child: Stack(
+            children: [
+              Text("HEllo There!",
+                  style:
+                      TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
+              Positioned(
+                  top: 15.0,
+                  child: Text("It's a silent sunnday!!!",
+                      style: TextStyle(
+                          fontSize: 10.0, fontWeight: FontWeight.bold))),
+              Positioned(
+                  bottom: 15.0,
+                  right: 0.0,
+                  child: Text("Hari PRem R C",
+                      style: TextStyle(
+                          fontSize: 10.0, fontWeight: FontWeight.bold))),
+              Positioned(
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: Text("signing off!",
+                      style: TextStyle(
+                          fontSize: 10.0, fontWeight: FontWeight.bold))),
+            ],
+          ),
         ),
       ),
     );
